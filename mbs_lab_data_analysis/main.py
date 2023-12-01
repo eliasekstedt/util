a63ed5bcedad437ae8bdfb45570b88c20912bbf7
eliasekstedt <ekstedt.mbt@gmail.com>
2023-12-01 11:49:17 +0100

    hejhopp

--- mbs_lab_data_analysis/main.py
+++ mbs_lab_data_analysis/main.py
@@ -3,41 +3,60 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 class Strain:
-    def __init__(self, strain_data, color):
+    def __init__(self, strain_data, background, color, label):
+        strain_data -= background
         self.mu = strain_data.mean()
         self.std = strain_data.std()
         self.n = len(self.mu)
         self.color = color
+        self.label = label
 
-def get_replicates(tag, row):
-    df = pd.read_csv(tag+'.csv')
-    return df[df['Well'].str.contains(str(row))]
+def get_replicates(tag, row, rm_replicates):
+    df = pd.read_csv(tag+'.csv', header=None)
+    for rep in rm_replicates:
+        df = df[~df[0].str.contains(str(rep))]    
+    return df[df[0].str.contains(str(row))]
+
+def get_background(tag):
+    df = pd.read_csv(tag+'.csv', header=None)
+    return df[df[0].str.contains(str('B'))].mean()
+
 
 
 def main():
     # read data
-    tag = 'placeholder'
+    tag = 'od600'
     
-    strain_1_data = get_replicates(tag, 10)
-    strain_2_data = get_replicates(tag, 11)
-    strain_3_data = get_replicates(tag, 8)
+    strain_1_data = get_replicates(tag, 10, ['A', 'B', 'H'])
+    strain_2_data = get_replicates(tag, 11, ['A', 'B', 'H'])
+    strain_3_data = get_replicates(tag, 8, ['A', 'B', 'H'])
+
+    background = get_background(tag)
 
     # do calculations
-    strain_1 = Strain(strain_1_data, color='blue')
-    strain_2 = Strain(strain_2_data, color='red')
-    strain_3 = Strain(strain_3_data, color='green')
-    
+    strain_1 = Strain(strain_1_data, background, 'blue', 'Empty Vector')
+    strain_2 = Strain(strain_2_data, background, 'red', 'Wild Type sRNA')
+    strain_3 = Strain(strain_3_data, background, 'green', 'Mutated sRNA-X')
+
+
+
+
     # plot
     strains = [strain_1, strain_2, strain_3]
 
     # plot option 1
     for strain in strains:
-        plt.plot(range(strain.n), strain.mu, strain.color)
+        plt.plot(range(strain.n), strain.mu, strain.color, label=strain.label)
         plt.fill_between(range(strain.n), strain.mu - 1*strain.std, strain.mu + 1*strain.std, alpha=0.3, color=strain.color)
-        plt.fill_between(range(strain.n), strain.mu - 2*strain.std, strain.mu + 2*strain.std, alpha=0.3, color=strain.color)
-    plt.savefig(f"{tag}_{'mu_&_std.png'}")
+        #plt.fill_between(range(strain.n), strain.mu - 2*strain.std, strain.mu + 2*strain.std, alpha=0.3, color=strain.color)
+    plt.title(f'{tag.upper()} Fluorescence Over Time')
+    plt.xlabel('Measurement')
+    plt.ylabel(f'{tag.upper()}')
+    plt.legend()
+    plt.savefig(f"{tag}_all_rep_{'mu_&_std.png'}")
     plt.figure()
 
+    """
     # plot option 2
     for strain in strains:
         plt.plot(range(strain.n), strain.mu, strain.color)
@@ -48,6 +67,8 @@ def main():
         plt.plot(range(strain.n), strain.std, strain.color)
     plt.savefig(f"{tag}_{'std.png'}")
     plt.figure()
+    """
+    
 
 
 if __name__ == '__main__':
